55705b836f37a279143cb7f08a9ff2c3b5e040b7
Menna ayman <mennaaymangalal4@gmail.com>
2025-04-23 17:02:46 +0200

    up

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-
-// export default function Footer() {
-//   return (
-//     <>
-//      <h1 className='text-center font-semibold text-2xl'>Footer</h1> 
-//     </>
-//   )
-// }
