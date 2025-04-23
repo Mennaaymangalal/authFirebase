74e70d937b7eb96e7b6c86425b9774391650db34
Menna ayman <mennaaymangalal4@gmail.com>
2025-04-24 00:17:39 +0200

    update

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavbarComp from '../../components/NavbarComp/NavbarComp'
 import { Outlet } from 'react-router-dom'
-import Footer from '../../components/Footer/Footer'
+
 
 export default function MainLayout() {
   return (
